4868a28f9cfe449b375934bd7bf69177eaf499fb
LucasBrito <lucasbsilva29@gmail.com>
2019-09-21 18:15:25 -0300

    ctrl + shift + i

--- black_back.py
+++ black_back.py
@@ -3,18 +3,18 @@ import numpy as np
 import utils as ut
 from matplotlib import pyplot as plt
 
-img = '/home/brito/Documentos/Dev/tcc/img/f1.jpeg'
+img = '/home/brito/Documentos/Dev/tcc/img/bicho.jpg'
 img = cv2.imread(img)
-mask = np.zeros(img.shape[:2],np.uint8)
+mask = np.zeros(img.shape[:2], np.uint8)
 
-bgdModel = np.zeros((1,65),np.float64)
-fgdModel = np.zeros((1,65),np.float64)
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
 
-rect = (430,196,800,310)
-cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+rect = (258, 164, 3484, 1916)  # (x,y,w,h)
+cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
-mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-img = img*mask2[:,:,np.newaxis]
+mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+img = img*mask2[:, :, np.newaxis]
 
 plt.imshow(img)
 plt.colorbar()
