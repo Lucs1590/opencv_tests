1dad23f67fe3575ea3022e8ad8dcbc5a725b745e
LucasBrito <lucasbsilva29@gmail.com>
2019-09-21 11:22:39 -0300

    cluster com Kmeans

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 def resize(img, scale_percent=50):
     width = int(img.shape[1] * (scale_percent / 100))
@@ -7,15 +9,40 @@ def resize(img, scale_percent=50):
     resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
     return resized
 
+
 def toSaturation(img):
-    hue, saturation, value = cv2.split(hsv)
+    hue, saturation, value = cv2.split(img)
     return saturation
+
 
 def toThreshold(sat):
     retval, thresholded = cv2.threshold(
         sat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return thresholded
 
+
 def toMedian(thresh):
     medianFiltered = cv2.medianBlur(thresh, 5)
     return medianFiltered
+
+
+def centroid_histogram(clt):
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+    hist = hist.astype("float")
+    hist /= hist.sum()
+    return hist
+
+
+def plot_colors(hist, centroids):
+    porcentagens = []
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+    for (percent, color) in zip(hist, centroids):
+        if color.astype("uint8").tolist() != [0, 0, 0]:
+            porcentagens.append(round(percent*100, 2))
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+    return bar, porcentagens
