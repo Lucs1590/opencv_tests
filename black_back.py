90432ca7cdc036b94acd52d088fb6fe8f6364b5a
LucasBrito <lucasbsilva29@gmail.com>
2019-09-24 22:16:54 -0300

    mudando a imagem e fazeendo plot

--- black_back.py
+++ black_back.py
@@ -5,12 +5,14 @@ from matplotlib import pyplot as plt
 
 img = '/home/brito/Documentos/Dev/tcc/img/bicho.jpg'
 img = cv2.imread(img)
+ut.img_plot(img)
+ut.img_show()
 mask = np.zeros(img.shape[:2], np.uint8)
 
 bgdModel = np.zeros((1, 65), np.float64)
 fgdModel = np.zeros((1, 65), np.float64)
 
-rect = (258, 164, 3484, 1916)  # (x,y,w,h)
+rect = (82, 51, 1118, 620)  # (x,y,w,h)
 cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
