cd1647e36e30cd0b24d414c467e385c39bf5b481
LucasBrito <lucasbsilva29@gmail.com>
2019-10-05 14:55:11 -0300

    Novas nomencaturas

--- reflect.py
+++ reflect.py
@@ -3,23 +3,26 @@ import numpy
 import utils as ut
 
 img = '/home/brito/Documentos/Dev/tcc/img/f1.jpeg'
-frame = cv2.imread(img)
+img = cv2.imread(img)
 
 
-if frame is None:
+if img is None:
     print('Error loading image')
     exit()
 
-rows = frame.shape[0]
-cols = frame.shape[1]
+rect = (430, 196, 800, 310)
+img = ut.black_back(img, rect)
 
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+rows = img.shape[0]
+cols = img.shape[1]
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 for i in range(0, cols):
     for j in range(0, rows):
         hsv[j, i][1] = 255
 
-# cv2.imshow("Frame 1", ut.resize(frame))
-frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-cv2.imshow("Frame 2", ut.resize(frame))
+# cv2.imshow("img 1", ut.resize(img))
+img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+cv2.imshow("img 2", ut.resize(img))
 cv2.waitKey(0)
