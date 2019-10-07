7337c9ac4d46b8ac4d6f743f320b4a61fec6e118
LucasBrito <lucasbsilva29@gmail.com>
2019-10-07 16:44:44 -0300

    nova imagem e comentario util

--- black_back.py
+++ black_back.py
@@ -3,14 +3,17 @@ import numpy as np
 import utils as ut
 from matplotlib import pyplot as plt
 
-img = '/home/brito/Documentos/Dev/tcc/img/bicho.jpg'
+img = '/home/brito/Documentos/Dev/tcc/img/f1.jpeg'
 img = cv2.imread(img)
 ut.img_plot(img)
 ut.img_show()
 
-rect = (82, 51, 1118, 620)  # (x,y,w,h)
+rect = (430, 196, 800, 310)
 img = ut.black_back(img, rect)
 
+
+""" cv2.imshow("GrabCut",img)
+cv2.waitKey(0) """
 plt.imshow(img)
 plt.colorbar()
 plt.show()
