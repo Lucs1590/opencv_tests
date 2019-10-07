352feb14e59a4dccfce5bc0d81a9f4bf987956e4
LucasBrito <lucasbsilva29@gmail.com>
2019-10-07 16:44:25 -0300

    rename e comentario util

--- utils.py
+++ utils.py
@@ -65,12 +65,13 @@ def black_back(img, coord):
     fgdModel = np.zeros((1, 65), np.float64)
     cv2.grabCut(img, mask, coord, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
     mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-    img = img*mask2[:, :, np.newaxis]
+    output = img*mask2[:, :, np.newaxis]
     """ cv2.imshow('img',ut.resize(img))
         cv2.imwrite('img.png',img)
         cv2.waitKey(0)
         cv2.destroyAllWindows() """
-    return img
+    return output
+    # return np.hstack([resize(img), resize(output)])
 
 
 def color_filter(img, color, diff):
