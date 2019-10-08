e2b6fae117a06c4a0bd3da3a4b37646bee710f54
LucasBrito <lucasbsilva29@gmail.com>
2019-10-07 21:05:51 -0300

    mudanças basicas

--- black_back.py
+++ black_back.py
@@ -5,18 +5,19 @@ from matplotlib import pyplot as plt
 
 img = '/home/brito/Documentos/Dev/tcc/img/f1.jpeg'
 img = cv2.imread(img)
-ut.img_plot(img)
-ut.img_show()
+img = ut.resize(img, 32)
+""" ut.img_plot(img)
+ut.img_show() """
 
 rect = (430, 196, 800, 310)
 img = ut.black_back(img, rect)
 
 
-""" cv2.imshow("GrabCut",img)
-cv2.waitKey(0) """
-plt.imshow(img)
+cv2.imshow("GrabCut", img)
+cv2.waitKey(0)
+""" plt.imshow(img)
 plt.colorbar()
-plt.show()
+plt.show() """
 """ cv2.imshow('img',ut.resize(img))
 cv2.imwrite('img.png',img)
 cv2.waitKey(0)
