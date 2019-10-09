d1d7de359334b9a74006e36fc24dd10aa73ad4cb
LucasBrito <lucasbsilva29@gmail.com>
2019-10-09 17:09:02 -0300

    mudando a foto

--- black_back.py
+++ black_back.py
@@ -3,15 +3,16 @@ import numpy as np
 import utils as ut
 from matplotlib import pyplot as plt
 
-img = '/home/brito/Documentos/Dev/tcc/img/f1.jpeg'
+img = '/home/brito/Documentos/Dev/tcc/img/f2.jpg'
 img = cv2.imread(img)
 img = ut.resize(img, 32)
 """ ut.img_plot(img)
 ut.img_show() """
 
-rect = (430, 196, 800, 310)
+rect = (80, 99, 1172, 565)
 img = ut.black_back(img, rect)
 
+""" img = ut.toHSV(img) """
 
 cv2.imshow("GrabCut", img)
 cv2.waitKey(0)
