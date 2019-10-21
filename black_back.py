11911a152ceb40833e695fa8c1dd3d1f91a3d669
LucasBrito <lucasbsilva29@gmail.com>
2019-10-20 22:43:42 -0300

    Novos parametros

--- black_back.py
+++ black_back.py
@@ -3,22 +3,22 @@ import numpy as np
 import utils as ut
 from matplotlib import pyplot as plt
 
-img = '/home/brito/Documentos/Dev/tcc/img/f2.jpg'
+img = '/home/brito/Documentos/Dev/tcc/img/f6.jpg'
 img = cv2.imread(img)
 img = ut.resize(img, 32)
-""" ut.img_plot(img)
-ut.img_show() """
+ut.img_plot(img)
+ut.img_show()
 
-rect = (80, 99, 1172, 565)
+rect = (46, 7, 1170, 687)
 img = ut.black_back(img, rect)
 
 """ img = ut.toHSV(img) """
 
-cv2.imshow("GrabCut", img)
-cv2.waitKey(0)
-""" plt.imshow(img)
+
+plt.imshow(img)
 plt.colorbar()
-plt.show() """
+plt.show()
+cv2.imwrite('f_b.png', img)
 """ cv2.imshow('img',ut.resize(img))
 cv2.imwrite('img.png',img)
 cv2.waitKey(0)
