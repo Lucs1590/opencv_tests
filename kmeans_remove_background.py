b0948f7cf3e92806c2eb42fce32d3415f880315d
Lucas Brito <lucasbsilva29@gmail.com>
2019-12-21 15:44:49 -0300

    to class

--- kmeans_remove_background.py
+++ kmeans_remove_background.py
@@ -1,32 +1,23 @@
 import cv2
-import numpy as np
 import utils as ut
 import operator
-from matplotlib import pyplot as plt
 from functools import reduce
-from sklearn.cluster import KMeans
 
 
-def toKmeans(img, clusters):
-    ut.img_plot(img)
-    img = img.reshape((img.shape[0] * img.shape[1], 3))
-    clt = KMeans(n_clusters=clusters)
-    clt.fit(img)
-    hist = ut.centroid_histogram(clt)
-    bar, porcentagens = ut.plot_colors(hist, clt.cluster_centers_)
-    return bar, porcentagens
+class KMeansClass(object):
+    def __init__(self):
+        ...
 
+    def runKmeans(self, img, rect, k):
+        img = cv2.imread(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        clusters = k
 
-img = 'resultados/f1_v.png'
-img = cv2.imread(img)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-rect = (430, 196, 800, 310)
-clusters = 5
-
-bar, porcentagens = toKmeans(ut.black_back(img, rect), clusters)
-contaminacao = (porcentagens[-1]*100)/reduce(operator.add, porcentagens)
-print(porcentagens)
-print("Folha + Doença: {}".format(round(reduce(operator.add, porcentagens), 2)))
-print("Contaminação de {}%".format(round(contaminacao, 2)))
-ut.img_plot(bar)
-ut.img_show()
+        bar, porcentagens = ut.toKmeans(ut.black_back(img, rect), clusters)
+        contaminacao = (porcentagens[-1]*100) / \
+            reduce(operator.add, porcentagens)
+        print(porcentagens)
+        print("Folha + Doença: {}".format(round(reduce(operator.add, porcentagens), 2)))
+        print("Contaminação de {}%".format(round(contaminacao, 2)))
+        ut.img_plot(bar)
+        ut.img_show()
